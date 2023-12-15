3d606369a96f86857b8a26c2b6a03f2c6d6e09a7
fksehf2 <fkstpdl@gmail.com>
2023-12-15 17:58:30 +0900

    component sacn filter

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member", //member패키지만 컴포넌트 대상
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 
 public class AutoAppConfig {
