475348d8bdee4b9fcd0740f1c1d7a22315a100e5
fksehf2 <fkstpdl@gmail.com>
2023-12-19 13:49:45 +0900

    AllBean test (lombok추가)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,11 +23,20 @@ public class OrderServiceImpl implements OrderService{
     * 필드 주입, @autowired private MemoryRepository memoryRepository 코드는 간결하지만 외부에서 변경이 불가능한 단점. 사용ㄴㄴ
     * 일반 메서드 주입, 한번에 여러 필드 주입 가능,
     */
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+
+//    lombok @RequiredArgsConstructor 가 final 붙은 field를 모아 생성자를 자동으로 생성해줌
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    /*
+    * autowired 매핑 규칙
+    * 1. 타입
+    * 2. 타입 매칭 결과가 2개 이상 일 때 필드 명이나 파라미터명으로 빈 이름 매칭
+    * 3. @Quillifier 사용
+    * 4. @Primary 사용 (우선 사용하겠다는 의미)
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
